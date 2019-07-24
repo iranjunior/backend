951c0d65c484254eea0d3e2f35e57eb88037bccd
iran junior <iranjunior94@gmail.com>
2019-07-23 22:13:09 -0300

    routes root

--- start/routes.js
+++ start/routes.js
@@ -39,7 +39,7 @@ Route.group(() => {
 
 Route.get("/", () => {
   return { welcome: "Olá mundo" };
-}).middleware(["auth"]);
+}).middleware(["guest"]);
 
 
 // Rotas para CRUD de hospitals com middleware de autenticação
